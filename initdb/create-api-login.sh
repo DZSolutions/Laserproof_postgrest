aa8b138e091c0077829402b29034edd114fd2837
!SharkPongsatorn <33623726+l3oaShark@users.noreply.github.com>
2024-11-02 00:36:59 +0700

    edit codition

--- initdb/create-api-login.sh
+++ initdb/create-api-login.sh
@@ -1,3 +1,8 @@
+
+# ถ้ามีฟังก์ชันนี้อยู่แล้วให้ลบก่อน
+DROP FUNCTION IF EXISTS public.login(TEXT, TEXT);
+
+
 CREATE OR REPLACE FUNCTION public.login(
     username TEXT,
     user_password TEXT
@@ -8,8 +13,8 @@ BEGIN
     -- ค้นหารหัสผ่านของผู้ใช้จาก email ที่ระบุ
     SELECT users.password INTO stored_password FROM users WHERE users.email = username;
 
-    -- ตรวจสอบว่ารหัสผ่านถูกต้อง
-    IF stored_password IS NOT NULL AND stored_password = user_password THEN
+    -- ตรวจสอบว่ารหัสผ่านถูกต้องโดยใช้ฟังก์ชัน crypt
+    IF stored_password IS NOT NULL AND crypt(user_password, stored_password) = stored_password THEN
         RETURN QUERY
         SELECT users.id, users.name, users.email, users.role
         FROM users
